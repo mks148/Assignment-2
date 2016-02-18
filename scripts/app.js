cfae6e3cef433b22958d70e6c7da3e90a12096d6
Manoj <mks148@gmail.com>
2016-02-18 17:31:53 -0500

    added code to app.js file for contact form

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,28 @@
 "use strict";
 /*Fucntion for contact page*/
-
-
-
-
+(function () {
+        if (document.getElementById("contact") != null) {
+        console.log("Contact Page");
+        
+        //Creating variables to store data
+        var name = document.getElementById("name");
+        var lname = document.getElementById("lname");
+        var email= document.getElementById("email");
+        var phone= document.getElementById("phone");
+        var message= document.getElementById("message");
+        var submitbtn = document.getElementById("submitbtn");
+        };
+//addevent lister for submit button and console log to show output
+        submitbtn.addEventListener("click", function(event){
+            event.preventDefault();
+            console.log("name: " + name.value);
+            console.log("lname: " + lname.value);
+            console.log("email: " + email.value);
+            console.log("phone: " + phone.value);
+            console.log("message: " + message.value);
+    
+        });
+})();
+        
+    
    
-  
